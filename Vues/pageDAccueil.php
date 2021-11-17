34e6a9a14af7424cf8684b3eaf9b2ff449d36f38
Antoine VITON <anviton@IUTCLINFA2319.iut.local>
2021-11-17 20:05:25 +0100

    Création du dossiers Vues et de la vue de la page d'accueil

---
+++ Vues/pageDAccueil.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<met charset="UTF-8"/>
+		<title>Accueil</title>
+		<link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <div class="container-fluid">
+		  	 <a class="navbar-brand" href="#">
+      			<img src="news.png" alt="" width="90" height="72" class="d-inline-block align-text-top">
+    		</a>
+		    <a class="navbar-brand" href="#">Navbar</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		      <div class="navbar-nav">
+		        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+		        <a class="nav-link" href="#">Admin</a>
+		        <a class="nav-link" href="#"></a>
+		        <a class="nav-link disabled"></a>
+		      </div>
+		    </div>
+		  </div>
+		</nav>
+		<table class="table table-dark table-hover">
+			News :
+			<thead>
+				<tr>
+					<th scope="col">Date et Heure</th>
+					<th scope="col">Site</th>
+					<th scope="col">Titre</th>
+					<th scope="col">Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td colspan="2">Larry the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+	<footer>
+				<p>Viton Antoine Dut informatique groupe7</p>
+	</footer>
+</html>
